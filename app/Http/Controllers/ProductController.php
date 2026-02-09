71f89d1f26604e49b78043ebb7ea0f97b3a0e09a
Yuji <ss.yuji1012@gmail.com>
2026-02-09 20:04:05 +0900

    ProductCRUDのルーティングとコントローラ作成

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $search_str = $request->input('search_str');
+
+        $products = Product::query()
+            ->when($search_str, function ($query, $search_str) {
+                $query->where('name', 'like', '%' . $search_str . '%');
+            })
+            ->get();
+
+        return Inertia::render('Products/Index', [
+            'products' => $products,
+            'search_str' => $search_str,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Products/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request)
+    {
+        Product::create($request->validated());
+
+        return redirect()->route('products.index')->with('success_str', '登録完了しました。');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return Inertia::render('Products/Edit', ['product' => $product]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return redirect()->route('products.index')->with('success_str', '更新完了しました。');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
+}
